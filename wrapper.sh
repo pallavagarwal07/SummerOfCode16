530cad70c3d0c9c0c0ab7cf1434d45fdd5eb086a
Pallav Agarwal <pallavag@iitk.ac.in>
2016-06-07 17:53:45 +0530

    Add cache directory support to wrapper script

--- wrapper.sh
+++ wrapper.sh
@@ -21,6 +21,23 @@ warn() {
     fi
 }
 
+normPath() {
+    dr=`expand_tilde "$1"` && mkdir -p $dr && cd $dr && pwd -P
+}
+
+expand_tilde()
+{
+    case "$1" in
+    (\~)        echo "$HOME";;
+    (\~/*)      echo "$HOME/${1#\~/}";;
+    (\~[^/]*/*) local user=$(eval echo ${1%%/*})
+                echo "$user/${1#*/}";;
+    (\~[^/]*)   eval echo ${1};;
+    (*)         echo "$1";;
+    esac
+}
+
+
 if [[ $EUID -eq 0 ]]; then
     error "This script should not be run using sudo or as the root user"
     exit 1
@@ -49,7 +66,24 @@ else
         if [[ "$docker" == "" ]];then docker=docker;else docker=${docker[0]};fi
         config="${config}docker=$docker\n"
 
-        echo -e "${config}" > "$filename"
+        printf "Do you want to keep a cache of downloaded packages (Y/n)? "
+        read -n 1 key
+        echo ""
+        if [[ "$key" == "y" || "$key" == "Y" || "$key" == "" ]]; then
+            config="${config}cache=1\n"
+            printf "In which folder do you want to store the cache? "
+            read cachePath
+            if normPath "$cachePath"; then
+                cachePath=`normPath "$cachePath"`
+            else
+                error "Path invalid or doesn't exist. Exiting."
+                exit 1
+            fi
+            config="${config}cachePath="$cachePath"\n"
+        else
+            config="${config}cache=0\n"
+        fi
+        head -c -1 <(echo -e "$config") > "$filename"
     else
         error "This script can't run without config file. Exiting."
     fi
@@ -57,7 +91,7 @@ fi
 
 if grep -Eqv '^#|^[^ ]*=[^\$; ]*[ 	]*$' "$filename"; then
     error "The config file is unclean. Trying to clean.."
-    tmp="$(grep -E '^#|^[^ ]*=[^\$; ]*[ ]*$' "$filename")"
+    tmp="$(grep -E '^#|^[^ ]*=[^\$; ]*[ ]*$|^[ 	]*$' "$filename")"
     echo "$tmp" > "$filename"
 fi
 
@@ -66,10 +100,24 @@ source "$filename"
 if [ -z "$docker" ]; then
     error "Config file faulty. Docker command not found in config file."
 else
-    if ! command -v $docker ; then
+    if ! command -v $docker >/dev/null 2>&1; then
         error "The executable $docker written in config - not found."
         exit 1
     fi
 fi
 
-$docker run -it pallavagarwal07/gentoo-stabilization
+if [ -z "$cache" ]; then
+    error "Config file faulty. Cache preference not found in config file."
+else
+    if [[ "$cache" == "1" ]] ; then
+        if [[ -z "$cachePath" ]]; then
+            error "Caching on, but cache path not defined in config file."
+        else
+            $docker run -v "$cachePath":/usr/portage/distfiles \
+                -it pallavagarwal07/gentoo-stabilization
+        fi
+    else
+        $docker run -it pallavagarwal07/gentoo-stabilization
+    fi
+fi
+
